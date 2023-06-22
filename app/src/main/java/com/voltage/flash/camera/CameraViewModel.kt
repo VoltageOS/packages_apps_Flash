822a8022566ba02f0fa3e6049294cc3804d578c3
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-06-22 23:20:38 +0200

    Flash: Introduce CameraViewModel
    Change-Id: I160b10e13be04d6ccdf2b3b623b59ed522e8adbc
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

---
+++ app/src/main/java/com/voltage/flash/camera/CameraViewModel.kt
@@ -0,0 +1,109 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.flash.camera
+
+import androidx.camera.video.Quality
+import androidx.camera.video.Recording
+import androidx.camera.view.video.AudioConfig
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.voltage.flash.utils.GridMode
+import com.voltage.flash.utils.Rotation
+import com.voltage.flash.utils.TimerMode
+
+/**
+ * [ViewModel] representing a camera session. This data is used to receive
+ * live data regarding the setting currently enabled.
+ */
+class CameraViewModel : ViewModel() {
+    // Base
+
+    /**
+     * The camera currently in use.
+     */
+    val camera = MutableLiveData<Camera>()
+
+    /**
+     * Current camera mode.
+     */
+    val cameraMode = MutableLiveData<CameraMode>()
+
+    /**
+     * Whether the current session is in single capture mode.
+     */
+    val inSingleCaptureMode = MutableLiveData(false)
+
+    /**
+     * Current camera state.
+     */
+    val cameraState = MutableLiveData<CameraState>()
+
+    /**
+     * Current screen rotation.
+     */
+    val screenRotation = MutableLiveData<Rotation>()
+
+    // General
+
+    /**
+     * Flash mode.
+     */
+    val flashMode = MutableLiveData(FlashMode.AUTO)
+
+    /**
+     * Grid mode.
+     */
+    val gridMode = MutableLiveData<GridMode>()
+
+    /**
+     * Timer mode.
+     */
+    val timerMode = MutableLiveData<TimerMode>()
+
+    // Photo
+
+    /**
+     * Photo capture mode.
+     */
+    val photoCaptureMode = MutableLiveData<Int>()
+
+    /**
+     * Photo aspect ratio.
+     */
+    val photoAspectRatio = MutableLiveData<Int>()
+
+    /**
+     * Photo effect.
+     */
+    val photoEffect = MutableLiveData<Int>()
+
+    // Video
+
+    /**
+     * Video quality.
+     */
+    val videoQuality = MutableLiveData<Quality>()
+
+    /**
+     * Video frame rate.
+     */
+    val videoFrameRate = MutableLiveData<FrameRate?>()
+
+    /**
+     * Video mic mode.
+     */
+    val videoMicMode = MutableLiveData<Boolean>()
+
+    /**
+     * Video [AudioConfig].
+     */
+    val videoAudioConfig = MutableLiveData<AudioConfig>()
+
+    /**
+     * Video [Recording].
+     */
+    val videoRecording = MutableLiveData<Recording?>()
+}
