6972dc48a33d2e4243d4e828d2e855483756b730
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-10 15:12:19 +0200

    Flash: Better handle left/right swipe gesture
    Change-Id: I4f6f108ce9f79f93c5e13165b8154ff0f74d4d70
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- app/src/main/java/com/voltage/flash/camera/CameraMode.kt
+++ app/src/main/java/com/voltage/flash/camera/CameraMode.kt
@@ -5,8 +5,20 @@
 
 package com.voltage.flash.camera
 
+import org.lineageos.aperture.ext.*
+
 enum class CameraMode {
     PHOTO,
     VIDEO,
-    QR,
+    QR;
+
+    /**
+     * Get the next mode.
+     */
+    fun next() = values().next(this)
+
+    /**
+     * Get the previous mode.
+     */
+    fun previous() = values().previous(this)
 }
