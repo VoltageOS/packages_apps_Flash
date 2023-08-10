statix-leddaz/android_packages_apps_Statix_Flash

Flash: Add volume buttons gestures
* Inspired by gcam

Change-Id: I69a2ae48ba7221dcf54b5efe538713a622dee886

app/src/main/java/com/statix/flash/CameraActivity.kt
@@ -60,6 +60,7 @@ import androidx.camera.view.video.AudioConfig
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.Group
+import androidx.core.animation.addListener
 import androidx.core.content.ContextCompat
 import androidx.core.location.LocationListenerCompat
 import androidx.core.location.LocationManagerCompat
@@ -80,6 +81,7 @@ import coil.request.ImageRequest
 import coil.request.SuccessResult
 import coil.size.Scale
 import com.google.android.material.button.MaterialButton
+import kotlinx.coroutines.sync.Mutex
 import com.statix.flash.camera.CameraFacing
 import com.statix.flash.camera.CameraManager
 import com.statix.flash.camera.CameraMode
@@ -103,6 +105,7 @@ import com.statix.flash.utils.AssistantIntent
 import com.statix.flash.utils.BroadcastUtils
 import com.statix.flash.utils.CameraSoundsUtils
 import com.statix.flash.utils.ExifUtils
+import com.statix.flash.utils.GestureActions
 import com.statix.flash.utils.GoogleLensUtils
 import com.statix.flash.utils.GridMode
 import com.statix.flash.utils.MediaStoreUtils
@@ -265,6 +268,8 @@ open class CameraActivity : AppCompatActivity() {
             field = value
             updateGalleryButton()
         }
+
+    private var zoomGestureMutex = Mutex()
 
     // Video
     private val supportedVideoQualities: Set<Quality>
@@ -914,13 +919,34 @@ open class CameraActivity : AppCompatActivity() {
                 }
                 true
             }
-            KeyEvent.KEYCODE_CAMERA,
-            KeyEvent.KEYCODE_VOLUME_UP,
-            KeyEvent.KEYCODE_VOLUME_DOWN -> {
+            KeyEvent.KEYCODE_CAMERA -> {
                 if (cameraMode == CameraMode.VIDEO && shutterButton.isEnabled && event?.repeatCount == 1) {
                     shutterButton.performClick()
                 }
                 true
+            }
+            KeyEvent.KEYCODE_VOLUME_UP,
+            KeyEvent.KEYCODE_VOLUME_DOWN -> when (sharedPreferences.volumeButtonsAction) {
+                GestureActions.SHUTTER -> {
+                    if (cameraMode == CameraMode.VIDEO && shutterButton.isEnabled && event?.repeatCount == 1) {
+                        shutterButton.performClick()
+                    }
+                    true
+                }
+                GestureActions.ZOOM -> {
+                    when (keyCode) {
+                        KeyEvent.KEYCODE_VOLUME_UP -> zoomIn()
+                        KeyEvent.KEYCODE_VOLUME_DOWN -> zoomOut()
+                    }
+                    true
+                }
+                GestureActions.VOLUME -> {
+                    super.onKeyDown(keyCode, event)
+                }
+                GestureActions.NOTHING -> {
+                    // Do nothing
+                    true
+                }
             }
             else -> super.onKeyDown(keyCode, event)
         }
@@ -930,13 +956,32 @@ open class CameraActivity : AppCompatActivity() {
         return if (capturePreviewLayout.isVisible) {
             super.onKeyUp(keyCode, event)
         } else when (keyCode) {
-            KeyEvent.KEYCODE_CAMERA,
-            KeyEvent.KEYCODE_VOLUME_UP,
-            KeyEvent.KEYCODE_VOLUME_DOWN -> {
+            KeyEvent.KEYCODE_CAMERA -> {
                 if (cameraMode != CameraMode.QR && shutterButton.isEnabled) {
                     shutterButton.performClick()
                 }
                 true
+            }
+            KeyEvent.KEYCODE_VOLUME_UP,
+            KeyEvent.KEYCODE_VOLUME_DOWN -> {
+                when (sharedPreferences.volumeButtonsAction) {
+                    GestureActions.SHUTTER -> {
+                        if (cameraMode != CameraMode.QR && shutterButton.isEnabled) {
+                            shutterButton.performClick()
+                        }
+                        true
+                    }
+                    GestureActions.ZOOM -> {
+                        true
+                    }
+                    GestureActions.VOLUME -> {
+                        super.onKeyDown(keyCode, event)
+                    }
+                    GestureActions.NOTHING -> {
+                        // Do nothing
+                        true
+                    }
+                }
             }
             else -> super.onKeyUp(keyCode, event)
         }
@@ -2092,6 +2137,58 @@ open class CameraActivity : AppCompatActivity() {
         galleryButtonCardView.smoothRotate(compensationValue)
         shutterButton.smoothRotate(compensationValue)
         flipCameraButton.smoothRotate(compensationValue)
+    }
+
+    /**
+     * Zoom in by a power of 2.
+     */
+    private fun zoomIn() {
+        val acquired = zoomGestureMutex.tryLock()
+        if (!acquired) {
+            return
+        }
+
+        val zoomState = cameraController.zoomState.value ?: return
+
+        ValueAnimator.ofFloat(
+            zoomState.zoomRatio,
+            zoomState.zoomRatio.nextPowerOfTwo().takeUnless {
+                it > zoomState.maxZoomRatio
+            } ?: zoomState.maxZoomRatio
+        ).apply {
+            addUpdateListener {
+                cameraController.setZoomRatio(it.animatedValue as Float)
+            }
+            addListener(onEnd = {
+                zoomGestureMutex.unlock()
+            })
+        }.start()
+    }
+
+    /**
+     * Zoom out by a power of 2.
+     */
+    private fun zoomOut() {
+        val acquired = zoomGestureMutex.tryLock()
+        if (!acquired) {
+            return
+        }
+
+        val zoomState = cameraController.zoomState.value ?: return
+
+        ValueAnimator.ofFloat(
+            zoomState.zoomRatio,
+            zoomState.zoomRatio.previousPowerOfTwo().takeUnless {
+                it < zoomState.minZoomRatio
+            } ?: zoomState.minZoomRatio
+        ).apply {
+            addUpdateListener {
+                cameraController.setZoomRatio(it.animatedValue as Float)
+            }
+            addListener(onEnd = {
+                zoomGestureMutex.unlock()
+            })
+        }.start()
     }
 
     fun preventClicks(@Suppress("UNUSED_PARAMETER") view: View) {}

app/src/main/java/com/statix/flash/ext/Float.kt
@@ -1,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.ext
+
+import kotlin.math.pow
+
+fun Float.previousPowerOfTwo(): Float {
+    if (this <= 0) return 0f
+
+    val power = kotlin.math.floor(kotlin.math.log2(this.toDouble())).toInt()
+    val result = 2.0.pow(power).toFloat()
+
+    return if (result >= this) {
+        result / 2
+    } else {
+        result
+    }
+}
+
+fun Float.nextPowerOfTwo(): Float {
+    if (this <= 0) return 0f
+
+    val power = kotlin.math.ceil(kotlin.math.log2(this.toDouble())).toInt()
+    val result = 2.0.pow(power).toFloat()
+
+    return if (result <= this) {
+        result * 2
+    } else {
+        result
+    }
+}

app/src/main/java/com/statix/flash/ext/SharedPreferences.kt
@@ -16,6 +16,7 @@ import com.statix.flash.camera.CameraFacing
 import com.statix.flash.camera.CameraMode
 import com.statix.flash.camera.FlashMode
 import com.statix.flash.camera.FrameRate
+import com.statix.flash.utils.GestureActions
 import com.statix.flash.utils.GridMode
 import com.statix.flash.utils.TimerMode
 
@@ -335,3 +336,16 @@ private const val VIDEO_STABILIZATION_KEY = "video_stabilization"
 private const val VIDEO_STABILIZATION_DEFAULT = true
 internal val SharedPreferences.videoStabilization: Boolean
     get() = getBoolean(VIDEO_STABILIZATION_KEY, VIDEO_STABILIZATION_DEFAULT)
+
+// Volume buttons action
+private const val VOLUME_BUTTONS_ACTION_KEY = "volume_buttons_action"
+private const val VOLUME_BUTTONS_ACTION_DEFAULT = "shutter"
+internal val SharedPreferences.volumeButtonsAction: GestureActions
+    get() = when (getString(VOLUME_BUTTONS_ACTION_KEY, VOLUME_BUTTONS_ACTION_DEFAULT)) {
+        "shutter" -> GestureActions.SHUTTER
+        "zoom" -> GestureActions.ZOOM
+        "volume" -> GestureActions.VOLUME
+        "nothing" -> GestureActions.NOTHING
+        // Default to shutter
+        else -> GestureActions.SHUTTER
+    }

app/src/main/java/com/statix/flash/utils/GestureActions.kt
@@ -1,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.utils
+
+/**
+ * Available gesture actions.
+ */
+enum class GestureActions {
+    SHUTTER,
+    ZOOM,
+    VOLUME,
+    NOTHING;
+}

VoltageOS/packages_apps_Flash

Signed-off-by: Dmitrii <bankersenator@gmail.com>

app/src/main/java/com/voltage/flash/CameraActivity.kt
@@ -60,6 +60,7 @@ import androidx.camera.view.video.AudioConfig
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.Group
+import androidx.core.animation.addListener
 import androidx.core.content.ContextCompat
 import androidx.core.location.LocationListenerCompat
 import androidx.core.location.LocationManagerCompat
@@ -80,6 +81,7 @@ import coil.request.ImageRequest
 import coil.request.SuccessResult
 import coil.size.Scale
 import com.google.android.material.button.MaterialButton
+import kotlinx.coroutines.sync.Mutex
 import com.voltage.flash.camera.CameraFacing
 import com.voltage.flash.camera.CameraManager
 import com.voltage.flash.camera.CameraMode
@@ -103,6 +105,7 @@ import com.voltage.flash.utils.AssistantIntent
 import com.voltage.flash.utils.BroadcastUtils
 import com.voltage.flash.utils.CameraSoundsUtils
 import com.voltage.flash.utils.ExifUtils
+import com.voltage.flash.utils.GestureActions
 import com.voltage.flash.utils.GoogleLensUtils
 import com.voltage.flash.utils.GridMode
 import com.voltage.flash.utils.MediaStoreUtils
@@ -265,6 +268,8 @@ open class CameraActivity : AppCompatActivity() {
             field = value
             updateGalleryButton()
         }
+
+    private var zoomGestureMutex = Mutex()
 
     // Video
     private val supportedVideoQualities: Set<Quality>
@@ -914,13 +919,34 @@ open class CameraActivity : AppCompatActivity() {
                 }
                 true
             }
-            KeyEvent.KEYCODE_CAMERA,
-            KeyEvent.KEYCODE_VOLUME_UP,
-            KeyEvent.KEYCODE_VOLUME_DOWN -> {
+            KeyEvent.KEYCODE_CAMERA -> {
                 if (cameraMode == CameraMode.VIDEO && shutterButton.isEnabled && event?.repeatCount == 1) {
                     shutterButton.performClick()
                 }
                 true
+            }
+            KeyEvent.KEYCODE_VOLUME_UP,
+            KeyEvent.KEYCODE_VOLUME_DOWN -> when (sharedPreferences.volumeButtonsAction) {
+                GestureActions.SHUTTER -> {
+                    if (cameraMode == CameraMode.VIDEO && shutterButton.isEnabled && event?.repeatCount == 1) {
+                        shutterButton.performClick()
+                    }
+                    true
+                }
+                GestureActions.ZOOM -> {
+                    when (keyCode) {
+                        KeyEvent.KEYCODE_VOLUME_UP -> zoomIn()
+                        KeyEvent.KEYCODE_VOLUME_DOWN -> zoomOut()
+                    }
+                    true
+                }
+                GestureActions.VOLUME -> {
+                    super.onKeyDown(keyCode, event)
+                }
+                GestureActions.NOTHING -> {
+                    // Do nothing
+                    true
+                }
             }
             else -> super.onKeyDown(keyCode, event)
         }
@@ -930,13 +956,32 @@ open class CameraActivity : AppCompatActivity() {
         return if (capturePreviewLayout.isVisible) {
             super.onKeyUp(keyCode, event)
         } else when (keyCode) {
-            KeyEvent.KEYCODE_CAMERA,
-            KeyEvent.KEYCODE_VOLUME_UP,
-            KeyEvent.KEYCODE_VOLUME_DOWN -> {
+            KeyEvent.KEYCODE_CAMERA -> {
                 if (cameraMode != CameraMode.QR && shutterButton.isEnabled) {
                     shutterButton.performClick()
                 }
                 true
+            }
+            KeyEvent.KEYCODE_VOLUME_UP,
+            KeyEvent.KEYCODE_VOLUME_DOWN -> {
+                when (sharedPreferences.volumeButtonsAction) {
+                    GestureActions.SHUTTER -> {
+                        if (cameraMode != CameraMode.QR && shutterButton.isEnabled) {
+                            shutterButton.performClick()
+                        }
+                        true
+                    }
+                    GestureActions.ZOOM -> {
+                        true
+                    }
+                    GestureActions.VOLUME -> {
+                        super.onKeyDown(keyCode, event)
+                    }
+                    GestureActions.NOTHING -> {
+                        // Do nothing
+                        true
+                    }
+                }
             }
             else -> super.onKeyUp(keyCode, event)
         }
@@ -2092,6 +2137,58 @@ open class CameraActivity : AppCompatActivity() {
         galleryButtonCardView.smoothRotate(compensationValue)
         shutterButton.smoothRotate(compensationValue)
         flipCameraButton.smoothRotate(compensationValue)
+    }
+
+    /**
+     * Zoom in by a power of 2.
+     */
+    private fun zoomIn() {
+        val acquired = zoomGestureMutex.tryLock()
+        if (!acquired) {
+            return
+        }
+
+        val zoomState = cameraController.zoomState.value ?: return
+
+        ValueAnimator.ofFloat(
+            zoomState.zoomRatio,
+            zoomState.zoomRatio.nextPowerOfTwo().takeUnless {
+                it > zoomState.maxZoomRatio
+            } ?: zoomState.maxZoomRatio
+        ).apply {
+            addUpdateListener {
+                cameraController.setZoomRatio(it.animatedValue as Float)
+            }
+            addListener(onEnd = {
+                zoomGestureMutex.unlock()
+            })
+        }.start()
+    }
+
+    /**
+     * Zoom out by a power of 2.
+     */
+    private fun zoomOut() {
+        val acquired = zoomGestureMutex.tryLock()
+        if (!acquired) {
+            return
+        }
+
+        val zoomState = cameraController.zoomState.value ?: return
+
+        ValueAnimator.ofFloat(
+            zoomState.zoomRatio,
+            zoomState.zoomRatio.previousPowerOfTwo().takeUnless {
+                it < zoomState.minZoomRatio
+            } ?: zoomState.minZoomRatio
+        ).apply {
+            addUpdateListener {
+                cameraController.setZoomRatio(it.animatedValue as Float)
+            }
+            addListener(onEnd = {
+                zoomGestureMutex.unlock()
+            })
+        }.start()
     }
 
     fun preventClicks(@Suppress("UNUSED_PARAMETER") view: View) {}

app/src/main/java/com/voltage/flash/ext/Float.kt
@@ -1,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.flash.ext
+
+import kotlin.math.pow
+
+fun Float.previousPowerOfTwo(): Float {
+    if (this <= 0) return 0f
+
+    val power = kotlin.math.floor(kotlin.math.log2(this.toDouble())).toInt()
+    val result = 2.0.pow(power).toFloat()
+
+    return if (result >= this) {
+        result / 2
+    } else {
+        result
+    }
+}
+
+fun Float.nextPowerOfTwo(): Float {
+    if (this <= 0) return 0f
+
+    val power = kotlin.math.ceil(kotlin.math.log2(this.toDouble())).toInt()
+    val result = 2.0.pow(power).toFloat()
+
+    return if (result <= this) {
+        result * 2
+    } else {
+        result
+    }
+}

app/src/main/java/com/voltage/flash/ext/SharedPreferences.kt
@@ -16,6 +16,7 @@ import com.voltage.flash.camera.CameraFacing
 import com.voltage.flash.camera.CameraMode
 import com.voltage.flash.camera.FlashMode
 import com.voltage.flash.camera.FrameRate
+import com.voltage.flash.utils.GestureActions
 import com.voltage.flash.utils.GridMode
 import com.voltage.flash.utils.TimerMode
 
@@ -335,3 +336,16 @@ private const val VIDEO_STABILIZATION_KEY = "video_stabilization"
 private const val VIDEO_STABILIZATION_DEFAULT = true
 internal val SharedPreferences.videoStabilization: Boolean
     get() = getBoolean(VIDEO_STABILIZATION_KEY, VIDEO_STABILIZATION_DEFAULT)
+
+// Volume buttons action
+private const val VOLUME_BUTTONS_ACTION_KEY = "volume_buttons_action"
+private const val VOLUME_BUTTONS_ACTION_DEFAULT = "shutter"
+internal val SharedPreferences.volumeButtonsAction: GestureActions
+    get() = when (getString(VOLUME_BUTTONS_ACTION_KEY, VOLUME_BUTTONS_ACTION_DEFAULT)) {
+        "shutter" -> GestureActions.SHUTTER
+        "zoom" -> GestureActions.ZOOM
+        "volume" -> GestureActions.VOLUME
+        "nothing" -> GestureActions.NOTHING
+        // Default to shutter
+        else -> GestureActions.SHUTTER
+    }

app/src/main/java/com/voltage/flash/utils/GestureActions.kt
@@ -1,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.flash.utils
+
+/**
+ * Available gesture actions.
+ */
+enum class GestureActions {
+    SHUTTER,
+    ZOOM,
+    VOLUME,
+    NOTHING;
+}