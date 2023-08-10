statix-leddaz/android_packages_apps_Statix_Flash

Flash: Use the view model for LensSelectorLayout
Change-Id: I0e26871364b715caeee2b7dc9701c06febb6f6f8

app/src/main/java/com/statix/flash/ui/LensSelectorLayout.kt
@@ -13,9 +13,13 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.Button
 import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.children
 import androidx.core.view.setMargins
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.statix.flash.R
 import com.statix.flash.camera.Camera
+import com.statix.flash.camera.CameraState
+import com.statix.flash.camera.CameraViewModel
 import com.statix.flash.ext.*
 import com.statix.flash.utils.Rotation
 import java.util.Locale
@@ -39,11 +43,33 @@ class LensSelectorLayout @JvmOverloads constructor(
     var onCameraChangeCallback: (camera: Camera) -> Unit = {}
     var onZoomRatioChangeCallback: (zoomRatio: Float) -> Unit = {}
 
-    internal var screenRotation = Rotation.ROTATION_0
+    internal var cameraViewModel: CameraViewModel? = null
         set(value) {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+
+            // Unregister
+            field?.cameraState?.removeObservers(lifecycleOwner)
+            field?.screenRotation?.removeObservers(lifecycleOwner)
+
             field = value
-            updateViewsRotation()
+
+            value?.let { cameraViewModel ->
+                cameraViewModel.cameraState.observe(lifecycleOwner) {
+                    val cameraState = it ?: return@observe
+
+                    children.forEach { view ->
+                        view.isSoundEffectsEnabled = cameraState == CameraState.IDLE
+                    }
+                }
+                cameraViewModel.screenRotation.observe(lifecycleOwner) {
+                    val screenRotation = it ?: return@observe
+
+                    updateViewsRotation(screenRotation)
+                }
+            }
         }
+    private val screenRotation
+        get() = cameraViewModel?.screenRotation?.value ?: Rotation.ROTATION_0
 
     fun setCamera(activeCamera: Camera, availableCameras: Collection<Camera>) {
         this.activeCamera = activeCamera
@@ -142,7 +168,7 @@ class LensSelectorLayout @JvmOverloads constructor(
         button.rotation = screenRotation.compensationValue.toFloat()
     }
 
-    private fun updateViewsRotation() {
+    private fun updateViewsRotation(screenRotation: Rotation) {
         val rotation = screenRotation.compensationValue.toFloat()
 
         for (button in buttonToApproximateZoomRatio.keys) {

VoltageOS/packages_apps_Flash

Signed-off-by: Dmitrii <bankersenator@gmail.com>

app/src/main/java/com/voltage/flash/ui/LensSelectorLayout.kt
@@ -13,9 +13,13 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.Button
 import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.children
 import androidx.core.view.setMargins
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.voltage.flash.R
 import com.voltage.flash.camera.Camera
+import com.voltage.flash.camera.CameraState
+import com.voltage.flash.camera.CameraViewModel
 import com.voltage.flash.ext.*
 import com.voltage.flash.utils.Rotation
 import java.util.Locale
@@ -39,11 +43,33 @@ class LensSelectorLayout @JvmOverloads constructor(
     var onCameraChangeCallback: (camera: Camera) -> Unit = {}
     var onZoomRatioChangeCallback: (zoomRatio: Float) -> Unit = {}
 
-    internal var screenRotation = Rotation.ROTATION_0
+    internal var cameraViewModel: CameraViewModel? = null
         set(value) {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+
+            // Unregister
+            field?.cameraState?.removeObservers(lifecycleOwner)
+            field?.screenRotation?.removeObservers(lifecycleOwner)
+
             field = value
-            updateViewsRotation()
+
+            value?.let { cameraViewModel ->
+                cameraViewModel.cameraState.observe(lifecycleOwner) {
+                    val cameraState = it ?: return@observe
+
+                    children.forEach { view ->
+                        view.isSoundEffectsEnabled = cameraState == CameraState.IDLE
+                    }
+                }
+                cameraViewModel.screenRotation.observe(lifecycleOwner) {
+                    val screenRotation = it ?: return@observe
+
+                    updateViewsRotation(screenRotation)
+                }
+            }
         }
+    private val screenRotation
+        get() = cameraViewModel?.screenRotation?.value ?: Rotation.ROTATION_0
 
     fun setCamera(activeCamera: Camera, availableCameras: Collection<Camera>) {
         this.activeCamera = activeCamera
@@ -142,7 +168,7 @@ class LensSelectorLayout @JvmOverloads constructor(
         button.rotation = screenRotation.compensationValue.toFloat()
     }
 
-    private fun updateViewsRotation() {
+    private fun updateViewsRotation(screenRotation: Rotation) {
         val rotation = screenRotation.compensationValue.toFloat()
 
         for (button in buttonToApproximateZoomRatio.keys) {