statix-leddaz/android_packages_apps_Statix_Flash

Flash: Add an info chip
* As of now it only shows low battery warning and video settings

Change-Id: I4a0d10b13a6a4de3cb3e6392179cbf38b137ca14

app/src/main/java/com/statix/flash/CameraActivity.kt
@@ -8,7 +8,10 @@ package com.statix.flash
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.app.KeyguardManager
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.ActivityInfo
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -100,6 +103,7 @@ import com.statix.flash.ui.CapturePreviewLayout
 import com.statix.flash.ui.CountDownView
 import com.statix.flash.ui.GridView
 import com.statix.flash.ui.HorizontalSlider
+import com.statix.flash.ui.InfoChipView
 import com.statix.flash.ui.LensSelectorLayout
 import com.statix.flash.ui.LevelerView
 import com.statix.flash.ui.LocationPermissionsDialog
@@ -146,6 +150,7 @@ open class CameraActivity : AppCompatActivity() {
     private val googleLensButton by lazy { findViewById<ImageButton>(R.id.googleLensButton) }
     private val gridButton by lazy { findViewById<Button>(R.id.gridButton) }
     private val gridView by lazy { findViewById<GridView>(R.id.gridView) }
+    private val infoChipView by lazy { findViewById<InfoChipView>(R.id.infoChipView) }
     private val lensSelectorLayout by lazy { findViewById<LensSelectorLayout>(R.id.lensSelectorLayout) }
     private val levelerView by lazy { findViewById<LevelerView>(R.id.levelerView) }
     private val micButton by lazy { findViewById<Button>(R.id.micButton) }
@@ -483,6 +488,14 @@ open class CameraActivity : AppCompatActivity() {
         }
     }
 
+    private val batteryBroadcastReceiver by lazy {
+        object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                infoChipView.batteryIntent = intent
+            }
+        }
+    }
+
     enum class ShutterAnimation(val resourceId: Int) {
         InitPhoto(R.drawable.avd_photo_capture),
         InitVideo(R.drawable.avd_mode_video_photo),
@@ -576,6 +589,7 @@ open class CameraActivity : AppCompatActivity() {
         // Pass the view model to the views
         capturePreviewLayout.cameraViewModel = model
         countDownView.cameraViewModel = model
+        infoChipView.cameraViewModel = model
 
         // Restore settings from shared preferences
         gridMode = sharedPreferences.lastGridMode
@@ -1152,6 +1166,9 @@ open class CameraActivity : AppCompatActivity() {
             powerManager.addThermalStatusListener(onThermalStatusChangedListener)
         }
 
+        // Start observing battery status
+        registerReceiver(batteryBroadcastReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+
         // Re-bind the use cases
         bindCameraUseCases()
     }
@@ -1167,6 +1184,9 @@ open class CameraActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             powerManager.removeThermalStatusListener(onThermalStatusChangedListener)
         }
+
+        // Remove battery status receiver
+        unregisterReceiver(batteryBroadcastReceiver)
 
         super.onPause()
     }

app/src/main/java/com/statix/flash/ui/InfoChipView.kt
@@ -1,0 +1,175 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.ui
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.content.Context
+import android.content.Intent
+import android.os.BatteryManager
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import com.statix.flash.R
+import com.statix.flash.camera.CameraMode
+import com.statix.flash.camera.CameraViewModel
+import com.statix.flash.utils.Rotation
+import kotlin.math.roundToInt
+
+class InfoChipView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    // Views
+    private val lowBatteryImageView by lazy { findViewById<ImageView>(R.id.lowBatteryImageView) }
+    private val videoMicMutedImageView by lazy { findViewById<ImageView>(R.id.videoMicMutedImageView) }
+
+    // System services
+    private val layoutInflater = context.getSystemService(LayoutInflater::class.java)
+
+    private val shortAnimationDuration by lazy {
+        resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+    }
+
+    internal var batteryIntent: Intent? = null
+        set(value) {
+            field = value
+
+            update()
+        }
+    internal var cameraViewModel: CameraViewModel? = null
+        set(value) {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+
+            field?.let {
+                // Unregister
+                it.screenRotation.removeObservers(lifecycleOwner)
+                it.cameraMode.removeObservers(lifecycleOwner)
+                it.videoMicMode.removeObservers(lifecycleOwner)
+            }
+
+            field = value
+
+            value?.let { cameraViewModel ->
+                cameraViewModel.screenRotation.observe(lifecycleOwner) {
+                    updateRotation()
+                }
+                cameraViewModel.cameraMode.observe(lifecycleOwner) {
+                    update()
+                }
+                cameraViewModel.videoMicMode.observe(lifecycleOwner) {
+                    update()
+                }
+            }
+        }
+
+    init {
+        layoutInflater.inflate(R.layout.info_chip_view, this)
+    }
+
+    private fun update() {
+        val cameraViewModel = cameraViewModel ?: return
+
+        val cameraMode = cameraViewModel.cameraMode.value ?: return
+        val videoMicMode = cameraViewModel.videoMicMode.value ?: return
+
+        // Get the new visibility values
+        val lowBatteryImageViewVisible = batteryIntent?.let {
+            val level = it.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = it.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+
+            val batteryPercentage = ((level * 100) / scale.toFloat()).roundToInt()
+
+            batteryPercentage <= 15
+        } ?: false
+        val videoMicMutedImageViewVisible = cameraMode == CameraMode.VIDEO && !videoMicMode
+
+        val shouldBeVisible = listOf(
+            lowBatteryImageViewVisible,
+            videoMicMutedImageViewVisible,
+        ).any { it }
+
+        val postUpdate = {
+            // Set the visibility on the views
+            lowBatteryImageView.isVisible = lowBatteryImageViewVisible
+            videoMicMutedImageView.isVisible = videoMicMutedImageViewVisible
+
+            updateRotation()
+        }
+
+        if (shouldBeVisible && !isVisible) {
+            postUpdate()
+
+            // Set the content view to 0% opacity but visible, so that it is visible
+            // (but fully transparent) during the animation.
+            alpha = 0f
+            isVisible = true
+
+            // Animate the content view to 100% opacity, and clear any animation
+            // listener set on the view.
+            animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration)
+                .setListener(null)
+        } else if (!shouldBeVisible && isVisible) {
+            animate()
+                .alpha(0f)
+                .setDuration(shortAnimationDuration)
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        isVisible = false
+
+                        postUpdate()
+                    }
+                })
+        } else {
+            postUpdate()
+        }
+    }
+
+    private fun updateRotation() {
+        val cameraViewModel = cameraViewModel ?: return
+
+        val screenRotation = cameraViewModel.screenRotation.value ?: return
+
+        val compensationValue = screenRotation.compensationValue.toFloat()
+
+        updateLayoutParams<LayoutParams> {
+            startToStart = when (screenRotation) {
+                Rotation.ROTATION_0,
+                Rotation.ROTATION_90,
+                Rotation.ROTATION_180 -> R.id.viewFinder
+                Rotation.ROTATION_270 -> LayoutParams.UNSET
+            }
+            endToEnd = when (screenRotation) {
+                Rotation.ROTATION_0,
+                Rotation.ROTATION_90,
+                Rotation.ROTATION_180 -> LayoutParams.UNSET
+                Rotation.ROTATION_270 -> R.id.viewFinder
+            }
+        }
+
+        rotation = compensationValue
+
+        measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+
+        translationX = when (screenRotation) {
+            Rotation.ROTATION_0,
+            Rotation.ROTATION_180 -> 0F
+            Rotation.ROTATION_90 -> -((measuredWidth - measuredHeight) / 2).toFloat()
+            Rotation.ROTATION_270 -> ((measuredWidth - measuredHeight) / 2).toFloat()
+        }
+        translationY = when (screenRotation) {
+            Rotation.ROTATION_0,
+            Rotation.ROTATION_180 -> 0F
+            Rotation.ROTATION_90,
+            Rotation.ROTATION_270 -> -((measuredHeight - measuredWidth) / 2).toFloat()
+        }
+    }
+}

VoltageOS/packages_apps_Flash

Signed-off-by: Dmitrii <bankersenator@gmail.com>

app/src/main/java/com/voltage/flash/CameraActivity.kt
@@ -8,7 +8,10 @@ package com.voltage.flash
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.app.KeyguardManager
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.ActivityInfo
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -100,6 +103,7 @@ import com.voltage.flash.ui.CapturePreviewLayout
 import com.voltage.flash.ui.CountDownView
 import com.voltage.flash.ui.GridView
 import com.voltage.flash.ui.HorizontalSlider
+import com.voltage.flash.ui.InfoChipView
 import com.voltage.flash.ui.LensSelectorLayout
 import com.voltage.flash.ui.LevelerView
 import com.voltage.flash.ui.LocationPermissionsDialog
@@ -146,6 +150,7 @@ open class CameraActivity : AppCompatActivity() {
     private val googleLensButton by lazy { findViewById<ImageButton>(R.id.googleLensButton) }
     private val gridButton by lazy { findViewById<Button>(R.id.gridButton) }
     private val gridView by lazy { findViewById<GridView>(R.id.gridView) }
+    private val infoChipView by lazy { findViewById<InfoChipView>(R.id.infoChipView) }
     private val lensSelectorLayout by lazy { findViewById<LensSelectorLayout>(R.id.lensSelectorLayout) }
     private val levelerView by lazy { findViewById<LevelerView>(R.id.levelerView) }
     private val micButton by lazy { findViewById<Button>(R.id.micButton) }
@@ -483,6 +488,14 @@ open class CameraActivity : AppCompatActivity() {
         }
     }
 
+    private val batteryBroadcastReceiver by lazy {
+        object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                infoChipView.batteryIntent = intent
+            }
+        }
+    }
+
     enum class ShutterAnimation(val resourceId: Int) {
         InitPhoto(R.drawable.avd_photo_capture),
         InitVideo(R.drawable.avd_mode_video_photo),
@@ -576,6 +589,7 @@ open class CameraActivity : AppCompatActivity() {
         // Pass the view model to the views
         capturePreviewLayout.cameraViewModel = model
         countDownView.cameraViewModel = model
+        infoChipView.cameraViewModel = model
 
         // Restore settings from shared preferences
         gridMode = sharedPreferences.lastGridMode
@@ -1152,6 +1166,9 @@ open class CameraActivity : AppCompatActivity() {
             powerManager.addThermalStatusListener(onThermalStatusChangedListener)
         }
 
+        // Start observing battery status
+        registerReceiver(batteryBroadcastReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+
         // Re-bind the use cases
         bindCameraUseCases()
     }
@@ -1167,6 +1184,9 @@ open class CameraActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             powerManager.removeThermalStatusListener(onThermalStatusChangedListener)
         }
+
+        // Remove battery status receiver
+        unregisterReceiver(batteryBroadcastReceiver)
 
         super.onPause()
     }

app/src/main/java/com/voltage/flash/ui/InfoChipView.kt
@@ -1,0 +1,175 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.flash.ui
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.content.Context
+import android.content.Intent
+import android.os.BatteryManager
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import com.voltage.flash.R
+import com.voltage.flash.camera.CameraMode
+import com.voltage.flash.camera.CameraViewModel
+import com.voltage.flash.utils.Rotation
+import kotlin.math.roundToInt
+
+class InfoChipView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    // Views
+    private val lowBatteryImageView by lazy { findViewById<ImageView>(R.id.lowBatteryImageView) }
+    private val videoMicMutedImageView by lazy { findViewById<ImageView>(R.id.videoMicMutedImageView) }
+
+    // System services
+    private val layoutInflater = context.getSystemService(LayoutInflater::class.java)
+
+    private val shortAnimationDuration by lazy {
+        resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+    }
+
+    internal var batteryIntent: Intent? = null
+        set(value) {
+            field = value
+
+            update()
+        }
+    internal var cameraViewModel: CameraViewModel? = null
+        set(value) {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+
+            field?.let {
+                // Unregister
+                it.screenRotation.removeObservers(lifecycleOwner)
+                it.cameraMode.removeObservers(lifecycleOwner)
+                it.videoMicMode.removeObservers(lifecycleOwner)
+            }
+
+            field = value
+
+            value?.let { cameraViewModel ->
+                cameraViewModel.screenRotation.observe(lifecycleOwner) {
+                    updateRotation()
+                }
+                cameraViewModel.cameraMode.observe(lifecycleOwner) {
+                    update()
+                }
+                cameraViewModel.videoMicMode.observe(lifecycleOwner) {
+                    update()
+                }
+            }
+        }
+
+    init {
+        layoutInflater.inflate(R.layout.info_chip_view, this)
+    }
+
+    private fun update() {
+        val cameraViewModel = cameraViewModel ?: return
+
+        val cameraMode = cameraViewModel.cameraMode.value ?: return
+        val videoMicMode = cameraViewModel.videoMicMode.value ?: return
+
+        // Get the new visibility values
+        val lowBatteryImageViewVisible = batteryIntent?.let {
+            val level = it.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = it.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+
+            val batteryPercentage = ((level * 100) / scale.toFloat()).roundToInt()
+
+            batteryPercentage <= 15
+        } ?: false
+        val videoMicMutedImageViewVisible = cameraMode == CameraMode.VIDEO && !videoMicMode
+
+        val shouldBeVisible = listOf(
+            lowBatteryImageViewVisible,
+            videoMicMutedImageViewVisible,
+        ).any { it }
+
+        val postUpdate = {
+            // Set the visibility on the views
+            lowBatteryImageView.isVisible = lowBatteryImageViewVisible
+            videoMicMutedImageView.isVisible = videoMicMutedImageViewVisible
+
+            updateRotation()
+        }
+
+        if (shouldBeVisible && !isVisible) {
+            postUpdate()
+
+            // Set the content view to 0% opacity but visible, so that it is visible
+            // (but fully transparent) during the animation.
+            alpha = 0f
+            isVisible = true
+
+            // Animate the content view to 100% opacity, and clear any animation
+            // listener set on the view.
+            animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration)
+                .setListener(null)
+        } else if (!shouldBeVisible && isVisible) {
+            animate()
+                .alpha(0f)
+                .setDuration(shortAnimationDuration)
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        isVisible = false
+
+                        postUpdate()
+                    }
+                })
+        } else {
+            postUpdate()
+        }
+    }
+
+    private fun updateRotation() {
+        val cameraViewModel = cameraViewModel ?: return
+
+        val screenRotation = cameraViewModel.screenRotation.value ?: return
+
+        val compensationValue = screenRotation.compensationValue.toFloat()
+
+        updateLayoutParams<LayoutParams> {
+            startToStart = when (screenRotation) {
+                Rotation.ROTATION_0,
+                Rotation.ROTATION_90,
+                Rotation.ROTATION_180 -> R.id.viewFinder
+                Rotation.ROTATION_270 -> LayoutParams.UNSET
+            }
+            endToEnd = when (screenRotation) {
+                Rotation.ROTATION_0,
+                Rotation.ROTATION_90,
+                Rotation.ROTATION_180 -> LayoutParams.UNSET
+                Rotation.ROTATION_270 -> R.id.viewFinder
+            }
+        }
+
+        rotation = compensationValue
+
+        measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+
+        translationX = when (screenRotation) {
+            Rotation.ROTATION_0,
+            Rotation.ROTATION_180 -> 0F
+            Rotation.ROTATION_90 -> -((measuredWidth - measuredHeight) / 2).toFloat()
+            Rotation.ROTATION_270 -> ((measuredWidth - measuredHeight) / 2).toFloat()
+        }
+        translationY = when (screenRotation) {
+            Rotation.ROTATION_0,
+            Rotation.ROTATION_180 -> 0F
+            Rotation.ROTATION_90,
+            Rotation.ROTATION_270 -> -((measuredHeight - measuredWidth) / 2).toFloat()
+        }
+    }
+}