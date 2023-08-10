statix-leddaz/android_packages_apps_Statix_Flash

Flash: Initial rebrand

app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.aperture"
+        applicationId = "com.statix.flash"
         minSdk = 26
         targetSdk = 33
         versionCode = 1

app/src/main/java/com/statix/flash/FlashApplication.kt
@@ -1,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class FlashApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Observe dynamic colors changes
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}

settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "Aperture"
+rootProject.name = "StatixCamera"
 include(":app")
 include(":lens_launcher")

VoltageOS/packages_apps_Flash

Signed-off-by: Dmitrii <bankersenator@gmail.com>

app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.aperture"
+        applicationId = "com.voltage.flash"
         minSdk = 26
         targetSdk = 33
         versionCode = 1

app/src/main/java/com/voltage/flash/FlashApplication.kt
@@ -1,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.flash
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class FlashApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Observe dynamic colors changes
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}

settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "Aperture"
+rootProject.name = "VoltageCamera"
 include(":app")
 include(":lens_launcher")